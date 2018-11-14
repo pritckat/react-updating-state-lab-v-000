950c16a9fb3ad443cde79c12923fe0cd015ce2f4
Katrina Pritchard <pritckat@gmail.com>
2018-11-14 19:42:07 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 class DigitalClicker extends React.Component{
 
+  constructor() {
+    super();
+
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+    this.setState((prevState) =>({
+      timesClicked: prevState.timesClicked + 1
+    })
+  )
+  }
+
   render() {
     return(
       <button onClick={this.handleClick}>{this.state.timesClickec}</button>
